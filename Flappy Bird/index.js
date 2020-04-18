fc8cd65ef12002f15c292d325cd314fe7e50e2de
vairamuthu shanmugavel <vairamuthu.sv@zohocorp.com>
2020-04-18 08:01:35 +0530

    Added validation and score

--- Flappy Bird/index.js
+++ Flappy Bird/index.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var animframe = window.requestAnimationFrame
-
+var animationId = null;
 var background = new Image();
 background.src = "assets/background-night.png";
 
@@ -17,6 +17,10 @@ bg.src = "assets/base.png";
 var bird = new Image();
 bird.src = "assets/bird.png"
 
+//game over image
+var gameover =  new Image();
+gameover.src = "assets/gameover.png"
+
 //Bird x and y position 
 var bx = 10;
 var by = canvas.height / 2
@@ -24,9 +28,13 @@ var by = canvas.height / 2
 
 var gap = 80;
 var gravity = 1;
-
+var score = 0;
+var isGameOver = false
 document.addEventListener("keydown", function () {
-    by -= 25
+    //try to avoiding the overshoot of bird.
+    if (by > 0 + bird.height && !isGameOver ) {
+        by -= 25
+    }
 })
 
 
@@ -40,7 +48,7 @@ pipes.push({
 
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0);
     for (let pipe of pipes) {
         ctx.drawImage(pipeTop, pipe.x, pipe.y);
@@ -53,21 +61,31 @@ function draw() {
             })
         }
         //detecting the collision
-        if ( ((bx + bird.width >= pipe.x && bx + bird.width <= pipe.x + pipeTop.width) &&
+        if (((bx + bird.width >= pipe.x && bx + bird.width <= pipe.x + pipeTop.width) &&
             (by <= pipe.y + pipeTop.height || by + bird.height > pipe.y + pipeTop.height + gap))
             || by + bird.height >= canvas.height - bg.height) {
-           console.log("collison")
+            // window.location.reload();
+            cancelAnimationFrame(animationId);
+            ctx.drawImage(gameover,50,canvas.height/2);
+            return;
+            
+        }
+
+        if (pipe.x === 3) {
+            score++;
         }
 
 
-
     }
+    ctx.fill = "#000";
+    ctx.font = "20px verdana";
+    ctx.fillText(`score : ${score}`, 10, 20);
     ctx.drawImage(bg, 0, canvas.height - bg.height);
     ctx.drawImage(bird, bx, by);
     //updating the birds why position based on  Gravity
     if ((by + bird.height) < (canvas.height - bg.height)) {
         by += gravity
     }
-    requestAnimationFrame(draw);
+  animationId =  requestAnimationFrame(draw);
 }
-draw()
+draw();
